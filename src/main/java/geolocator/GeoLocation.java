64eacb0e3e7e887937368831947e9a68d6e39e4a
Liusongzhi27 <z15628955277@gmail.com>
2022-03-02 00:09:35 +0100

    Implementing toString() in GeoLocation using ReflectionToStringBuilder

--- src/main/java/geolocator/GeoLocation.java
+++ src/main/java/geolocator/GeoLocation.java
@@ -1,5 +1,10 @@
 package geolocator;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
+public String toString() {
+        return ReflectionToStringBuilder.toString(this, ToStringStyle.MULTI_LINE_STYLE);
+        }
 public class GeoLocation {
 
     private String as;
